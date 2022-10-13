ad6332f06f29c3f0e64878247df6605ef6184e70
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-13 16:30:35 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,22 @@
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-return (0);
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+
+}
+
+
+
+
