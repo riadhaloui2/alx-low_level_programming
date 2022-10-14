1b185c137861d1b93ac88e472b21a2c008291010
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-13 17:19:42 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*
  * main - Entry point
+ *
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
@@ -9,6 +10,6 @@ int main(void)
 
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 
 }
