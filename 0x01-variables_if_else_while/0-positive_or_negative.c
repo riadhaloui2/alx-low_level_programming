f3c3c61eb31931e5d645c38b841886729f95bf4a
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-15 07:30:47 -0700

    go

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,12 +9,16 @@
 
 
 /* betty style doc for function main goes there */
-/*
- * main - Entry point
- *
- * Description :'if statement'
- * Return: Always 0 (Success)
- */
+ /**
+  *
+  * main - Entry point
+  *
+  * 
+  * Description: 'the program's description'
+  *
+  * 
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 		int n;
