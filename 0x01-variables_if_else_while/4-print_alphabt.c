3cdbae750de8d6442691a98115a79f4907f31c6a
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-16 05:33:21 -0700

    lower

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Description: put the lower
+ *with upper case alphabet
+ *Return: Always 0 (Success)
+ */
+
+
+
+int main(void)
+
+{
+
+		int ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+			if (ch != 'e' && ch != 'q')
+			putchar(ch);
+		
+		putchar('\n');
+
+		return (0);
+
+}
