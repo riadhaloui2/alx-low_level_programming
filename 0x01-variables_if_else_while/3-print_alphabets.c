f417d21340979bc7b21d8a47c124f9653c5316f2
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-16 04:35:24 -0700

    print upper case alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
-#include <stdio.h>                                                                                                          
+#include <stdio.h>
 
-/**                                           
+/**
  * main - Entry point                                                            *Description: put the lower with upper case alphabet
  *Return: Always 0 (Success)                                                     *
- */                                                
+ */
 
-                                                                                                                            
 
-int main(void)                                                                                                              
 
-{                                                                                                                           
+int main(void)
 
-	        int ch;                                                                                                             
+{
 
-		for (ch = 'a' ; ch <= 'z' ; ch++)                                                                                   
+	        int ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
 			putchar(ch);
 		for (ch = 'A' ; ch <= 'Z' ; ch++)
-			putchar(ch);		
-	        putchar('\n');                                                                                                      
+			putchar(ch);
+	        putchar('\n');
 
-		return (0);                                                                                                         
+		return (0);
 
-}    
+}
