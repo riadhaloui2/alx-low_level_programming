d5dac4ce0cd4b7a70026cf81ede4f9f4812f9455
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-18 08:34:54 -0700

    sss

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print hexa numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 8 ; n++)
+		putchar (n % 10 + '0');
+		putchar (" ,");
+	putchar (n % 9 + '0');	
+	putchar ('\n');
+
+
+
+
+
+	return (0);
+}
