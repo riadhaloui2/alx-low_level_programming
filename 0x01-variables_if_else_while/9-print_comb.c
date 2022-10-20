ca9aba6448ec7ddb6d548831083da9993b1a77b6
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-20 12:46:34 -0700

    cvv

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,7 @@ int main(void) {
 		for (n = 0; n <= 9; n++)
 		{                           
 			putchar (n +48);                     
-			putchar (" ");
+			putchar ("\t");
 				        
 			if (n != 9 )
 			{
