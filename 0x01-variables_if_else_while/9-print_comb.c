ed248aa325f79c0e726a209da23f0bccf518b4fa
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-20 12:26:18 -0700

    gi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,23 +6,20 @@
  * Return: Always 0 (success)
  */
 
-int main(void)
-{
-	int n;
 
-	for (n = 0; n <= 9; n++)
-	{
-
-		putchar (n + '0');                     		                		putchar (" ");
-
-		if (n != 9)
-		{
-			putchar (",");
-		}
-	}	
-	putchar ("\n");
-	
-
-
-	return (0);
+int main(void) {
+		int n;                                                                                                          
+		for (n = 0; n <= 9; n++)
+		{                           
+			putchar (n + '0');                     
+			putchar (" ");
+				        
+			if (n != 9 )
+			{
+				putchar (',');								}
+		} 
+			          
+			                                                                                                          
+		putchar ('\n'); 
+		return (0);
 }
