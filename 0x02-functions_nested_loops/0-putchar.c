58132862e721a18cd13bb485497199d34cf4cccb
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-22 08:42:54 -0700

    putmaraokhra

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
-include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
-	char word [] = {'_','p','u','t','c','h','a','r'};
-	for(int i = 0; i <= 7 ;i++)
-		putchar(word[i]);
-	putchar("\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return(0);
-
 
 
 
