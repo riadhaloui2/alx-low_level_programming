e00d796fed856f77aa0f88e259f3b4a811684fcd
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-22 09:17:30 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ * Description: print if the number is positive or negative
+ * Return: Always 0 (Success)
+ */
+
+int print_alphabet()
+{
+	for(int i = a; i <= z; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+
+
+}
+int main(void)
+{
+
+print_alphabet();
+return (0);
+
+
+
+
+}
