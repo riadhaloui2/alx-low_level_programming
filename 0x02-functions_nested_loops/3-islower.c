1ddd5b5231512a1b11d5729dc3af87e4cb2b7de3
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-22 12:01:15 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _islower - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+
+
+
+
+}
+
+
