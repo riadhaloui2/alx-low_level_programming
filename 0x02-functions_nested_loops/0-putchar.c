5183b3d35c2a3e8771001b98a7c57af5fa631f2b
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-22 08:50:41 -0700

    mara

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include 'main.h'
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -6,17 +6,17 @@
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	return(0);
+	return (0);
 
 
 
