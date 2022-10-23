e9a376c5f580ee5c17bc26431ecf72208b2681a2
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-23 06:15:10 -0700

    you b roo

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute
+ * @n: The character to print
+ * Return: On success 1.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+		return (n);
+	return (0);
+
+
+}
+
