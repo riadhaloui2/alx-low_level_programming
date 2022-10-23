24bc38dc9d6817a8706b408cfc10f738ec9cf58a
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-23 03:10:15 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if the char is an alphabet
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+
+int _isalpha(int c)
+{
+	int c;
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+
+
+
+
+
+
+}
