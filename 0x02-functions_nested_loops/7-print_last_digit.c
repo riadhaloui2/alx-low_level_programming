940dbcce39c487e2e4e7e77d39c5a3543e030a46
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-23 06:31:18 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print
+ * @n: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+int print_last_digit(int n)
+{
+
+	_putchar (n % 10 + '0');
+	return (0);
+
+
+
+}
