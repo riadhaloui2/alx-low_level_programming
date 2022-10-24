67476907d94ec7d4527b780c860d02d6c44b68e5
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-24 06:44:25 -0700

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; y++)
+		for (b = 0; b <= 9; b++)
 		{
-			c = x * y;
+			c = a * b;
 			if ((c / 10) == 0)
 			{
 				if (b == 0)
