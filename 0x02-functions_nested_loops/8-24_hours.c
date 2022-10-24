c7b431f5fa954bed54ba930f29371999f21ec30c
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-24 02:15:23 -0700

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *jack_bauer - print time
+ */
+
+void jack_bauer(void)
+{
+
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if ((a >= 50) && (b  >= 52))
+						break;
+					_putchar (a);
+					_putchar (b);
+					_putchar (58);
+					_putchar (c);
+					_putchar (d);
+					_putchar ('\n');
+
+				}
+			}
+		}
+	}
+
+	return (0);
+
+}
