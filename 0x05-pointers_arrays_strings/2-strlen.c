e465f5cfa4cd789994efe820387de1e31f46b690
riadhaloui2 <riadhhaloui@gmail.com>
2022-10-28 09:36:12 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/** 
- * _strlen - Returns the length of a string.
- * @str: string.
- *
- * Return: length.
+/**
+ * _strlen - calculates the length of a string.
+ * @s: pointer to the character array.
+ * Return: length of the string
  */
 
 int _strlen(char *s)
