f970d75547958fb5dc283330c6dcd8710e02eb52
riadhaloui2 <riadhhaloui@gmail.com>
2022-11-02 10:18:44 -0700

    2strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - Entry point
+ * @s:input
+ * @c:input
+ * Return: Always 0 (Success)
+ */
+char _strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i <= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+
+
+	}
+
+	return (NULL);
+
+
+
+
+}
+
