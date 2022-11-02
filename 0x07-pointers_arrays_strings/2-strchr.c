9e214e136efce83a6b1a427842a14194d366094e
riadhaloui2 <riadhhaloui@gmail.com>
2022-11-02 10:25:42 -0700

    2strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,21 +8,24 @@
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
+
 {
+
 	int i;
 
-	for (i = 0; i <= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (s + i);
 
+
+	for (i = 0; s[i] >= '\0'; i++)
+
+	{
+
+		if (s[i] == c)
+
+			return (s + i);
 
 	}
 
+
 	return (NULL);
 
-
-
-
 }
-
