f49c3bf65460d4dc3b3a38b94ab272b68e9531a5
riadhaloui2 <riadhhaloui@gmail.com>
2022-11-11 00:19:27 -0800

    ecursion0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion print on the screen
+ * @s: input
+ * return:success(0)
+ */
+
+void _puts_recursion(char *s);
+{
+	int i;
+
+	for(i = 0, i < '\0', i++)
+	{
+		_putchar(s[i]);
+	
+	}
+	_putchar("\n");
+
+
+}
